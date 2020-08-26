4fe91f22f91835ebe91a9c4a7df667763992e224
gheezl <gheezl25@gmail.com>
2020-08-26 16:07:33 -0400

    Added some javascript files and styling

---
+++ javascript/game.js
@@ -0,0 +1,26 @@
+import { SNAKE_SPEED, updateSnake, drawSnake } from "./snake.js"
+
+let lastRenderTime = 0
+
+const main = (currentTime) => {
+    window.requestAnimationFrame(main)
+    const secondsSinceLastRender = (currentTime - lastRenderTime) / 1000
+
+    if (secondsSinceLastRender < 1 / SNAKE_SPEED) return
+
+    lastRenderTime = currentTime
+    console.log("hi")
+
+    update()
+    draw()
+}
+
+window.requestAnimationFrame(main)
+
+const update = () => {
+    updateSnake()
+}
+
+const draw = () => {
+    drawSnake()
+}
---
+++ javascript/snake.js
@@ -0,0 +1,9 @@
+export const SNAKE_SPEED = 5
+
+export const updateSnake = () => {
+    console.log("update")
+}
+
+export const drawSnake = () => {
+    console.log("draw")
+}
